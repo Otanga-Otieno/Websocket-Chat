67fc00b023d91592c2f3a36dab0639e35d07c847
Otanga-Otieno <otieno.otanga@gmail.com>
2022-04-13 16:12:43 +0300

    Implement app.js as application entry point

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 var static = require('node-static');
+var url = require('url');
 
-var file = new (static.Server)("ui/");
+var file = new static.Server('./ui');
 
 var server = http.createServer(function(req, res) {
+    if(req.url === "/") req.url = "/index.htm";
     file.serve(req, res);
 });
-server.listen(8001); 
+server.listen(8001);
